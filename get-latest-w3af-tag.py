c23a14205c44385dfbc8672ac2f46e8edba04927
Andres Riancho <andres.riancho@gmail.com>
2015-04-22 09:52:51 -0300

    Fixed script to get the latest tag according to semver

--- get-latest-w3af-tag.py
+++ get-latest-w3af-tag.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
 import requests
+import semver
 
-url = 'https://api.github.com/repos/andresriancho/w3af/releases'
+url = 'https://api.github.com/repos/andresriancho/w3af/tags'
 
 data = requests.get(url).json()
-print data[0]['tag_name']
+tag_names = [x['name'] for x in data]
+
+valid_tags = []
+
+for tag_name in tag_names:
+    try:
+        semver.parse(tag_name)
+    except ValueError:
+        pass
+    else:
+        valid_tags.append(tag_name)
+
+valid_tags.sort(semver.compare)
+print valid_tags[-1]
